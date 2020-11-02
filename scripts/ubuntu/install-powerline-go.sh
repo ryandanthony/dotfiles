881e166226fea4b7d97eaaf1366b3dbb666ad570
Ryan Anthony <ryandanthony@users.noreply.github.com>
2020-11-02 12:26:53 -0700

    get powerline from release, don't build

---
+++ scripts/ubuntu/install-powerline-go.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+version=v1.18.0
+curl -L https://github.com/justjanne/powerline-go/releases/download/$version/powerline-go-linux-amd64 -o ~/go/bin/powerline-go
+chmod +x ~/go/bin/powerline-go
