95e41144472df74f1c1472348c2f2d59f9b77a7f
Ryan Anthony <ryandanthony@users.noreply.github.com>
2020-11-12 14:23:53 -0700

    adding node

--- scripts/ubuntu/install-keybase-io.sh
+++ scripts/ubuntu/install-keybase-io.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 curl -L https://prerelease.keybase.io/keybase_amd64.deb -o ~/Downloads/keybase_amd64.deb
 sudo apt install ~/Downloads/keybase_amd64.deb -y
-run_keybase
+#run_keybase
---
+++ scripts/ubuntu/install-nodejs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install nodejs -y
+node --version
+npm --version
