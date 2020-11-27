3f32ca607afb5d62d66b98155d58cfafa6661809
Ryan Anthony <ryandanthony@hotmail.com>
2020-11-27 16:54:18 -0700

    fixed powerline install

--- scripts/ubuntu/install-powerline-go.sh
+++ scripts/ubuntu/install-powerline-go.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+mkdir -p ~/go/bin/
 version=v1.18.0
 curl -L https://github.com/justjanne/powerline-go/releases/download/$version/powerline-go-linux-amd64 -o ~/go/bin/powerline-go
 chmod +x ~/go/bin/powerline-go
