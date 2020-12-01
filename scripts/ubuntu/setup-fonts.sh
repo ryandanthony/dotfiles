282ee19468d4c831e4444dbcb9fdb4f3cc276a8b
Ryan Anthony <ryandanthony@users.noreply.github.com>
2020-12-01 12:05:56 -0700

    setup fonts part 2

--- scripts/ubuntu/setup-fonts.sh
+++ scripts/ubuntu/setup-fonts.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Ubuntu Mono derivative Powerline 11"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type=boolean false
