36f668a52d270c4b451bc1b14bebb393f9b036ab
Ryan Anthony <ryandanthony@users.noreply.github.com>
2021-02-14 19:28:59 +0000

    adding kubectl

--- powershell_profile.ps1
+++ powershell_profile.ps1
@@ -1,6 +1,17 @@
 #!/usr/bin/env pwsh
 # Load powerline-go prompt
 Write-Host "Hello!"
+# microk8s status 2>&1 | out-null
+# if ($?)
+# {
+#     Write-Host "microk8s is installed. Using it's version of kubectl and helm3"
+#     microk8s enable kubectl 2>&1 | out-null
+#     microk8s enable helm3 2>&1 | out-null
+
+#     Set-Alias -Name kubectl -Value microk8s.kubectl
+#     Set-Alias -Name helm -Value microk8s.helm3
+# }
+
 function global:prompt {
     $standardOut = $(~/go/bin/powerline-go -shell bare)
     $standardOut
