872466e5628d70a10072d67411c29fc6dbacaeec
Ryan Anthony <ryandanthony@users.noreply.github.com>
2021-02-14 18:58:55 +0000

    font corrections

--- scripts/ubuntu/setup-fonts.sh
+++ scripts/ubuntu/setup-fonts.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+fc-cache -f -v
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "Ubuntu Mono derivative Powerline 11"
 gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type=boolean false
