3135dfe5762eeb0aa9032053ad1c2051b594131e
Ryan Anthony <ryandanthony@users.noreply.github.com>
2021-04-14 22:52:30 +0000

    update versions of dotnet installed

--- scripts/ubuntu/install-dotnet-sdks.sh
+++ scripts/ubuntu/install-dotnet-sdks.sh
@@ -10,4 +10,4 @@ else
 fi
 
 sudo apt-get update;
-sudo apt-get install dotnet-sdk-5.0 dotnet-sdk-3.1 dotnet-sdk-2.1 -y
+sudo apt-get install dotnet-sdk-5.0 dotnet-sdk-3.1=3.1.110-1 dotnet-sdk-2.1 -y
